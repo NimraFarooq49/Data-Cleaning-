a7932d9d9da62fe3a442cec74c380f35069f8f05
Nimra <jaffarnimra5@gmail.com>
2026-07-07 07:51:58 -0700

    Updated duplicate row removal function

--- cleaner.py
+++ cleaner.py
@@ -14,50 +14,63 @@ class LoadData:
 class DataCleaner:
     def __init__(self, df):
         self.df = df.copy()
+    def remove_duplicates(self):
+        before = len(self.df)
 
-    def remove_duplicate(self):
-        self.df = self.df.drop_duplicates().copy()
-        return self.df
+        self.df = self.df.drop_duplicates().reset_index(drop=True)
 
-    def missing_value(self):
-        return self.df.isnull().sum()
+        after = len(self.df)
 
-    def fill_missing(self):
-        self.df["Age"] = self.df["Age"].fillna(self.df["Age"].mean())
-        self.df["Fare"] = self.df["Fare"].fillna(self.df["Fare"].mean())
-        self.df = self.df.fillna("Unknown")
-        return self.df
-
-    def remove_whitespace(self):
-        self.df = self.df.apply(
-            lambda x: x.str.strip() if x.dtype == "object" else x
-        )
-        return self.df
-
-    def clean_invalid_email(self):
-        self.df["Email"] = self.df["Email"].fillna("unknown@gmail.com")
-
-        self.df["Email"] = self.df["Email"].apply(
-            lambda x: x if ("@" in str(x) and "." in str(x))
-            else "unknown@gmail.com"
-        )
+        print(f"Duplicate rows removed: {before - after}")
 
         return self.df
 
 
-class EDAanalysis:
-    def __init__(self, df):
-        self.df = df
 
-    def preview(self):
-        return self.df.head()
 
-    def shape(self):
-        return self.df.shape
+#     def remove_duplicate(self):
+#         self.df = self.df.drop_duplicates().copy()
+#         return self.df
 
-    def columns(self):
-        return self.df.columns
+#     def missing_value(self):
+#         return self.df.isnull().sum()
 
-    def info(self):
-        return self.df.info()
+#     def fill_missing(self):
+#         self.df["Age"] = self.df["Age"].fillna(self.df["Age"].mean())
+#         self.df["Fare"] = self.df["Fare"].fillna(self.df["Fare"].mean())
+#         self.df = self.df.fillna("Unknown")
+#         return self.df
+
+#     def remove_whitespace(self):
+#         self.df = self.df.apply(
+#             lambda x: x.str.strip() if x.dtype == "object" else x
+#         )
+#         return self.df
+
+#     def clean_invalid_email(self):
+#         self.df["Email"] = self.df["Email"].fillna("unknown@gmail.com")
+
+#         self.df["Email"] = self.df["Email"].apply(
+#             lambda x: x if ("@" in str(x) and "." in str(x))
+#             else "unknown@gmail.com"
+#         )
+
+#         return self.df
+
+
+# class EDAanalysis:
+#     def __init__(self, df):
+#         self.df = df
+
+#     def preview(self):
+#         return self.df.head()
+
+#     def shape(self):
+#         return self.df.shape
+
+#     def columns(self):
+#         return self.df.columns
+
+#     def info(self):
+#         return self.df.info()
 
--- main.py
+++ main.py
@@ -1,48 +1,57 @@
-from cleaner import LoadData, DataCleaner, EDAanalysis
-from exporter import ExportData
+from cleaner import LoadData, DataCleaner 
+#  EDAanalysis
+# from exporter import ExportData
 
 
 # Load Data
 loader = LoadData("input/data.csv")
 df = loader.load_data()
 
-
-# EDA
-eda = EDAanalysis(df)
-
-print(eda.preview())
-print("Shape:", eda.shape())
-print("Columns:", eda.columns())
-
-eda.info()
-
-
 # Cleaning
 cleaner = DataCleaner(df)
 
-print("Missing Values:\n")
-print(cleaner.missing_value())
+df = cleaner.remove_duplicates()
 
-df = cleaner.remove_duplicate()
-df = cleaner.fill_missing()
-df = cleaner.remove_whitespace()
-df = cleaner.clean_invalid_email()
+print("\nDataset After Removing Duplicates:")
+print(df)
 
-print("\nCleaning Done")
 
-cleaner = DataCleaner(df)
+# EDA
+# eda = EDAanalysis(df)
 
-print("\nMissing Values:\n")
-print(cleaner.missing_value())
+# print(eda.preview())
+# print("Shape:", eda.shape())
+# print("Columns:", eda.columns())
 
-print("\nFirst 5 Rows After Cleaning:")
-print(df.head())
+# eda.info()
+
+
+# Cleaning
+
+
+# print("Missing Values:\n")
+# print(cleaner.missing_value())
+
+    #  df = cleaner.remove_duplicates()
+# df = cleaner.fill_missing()
+# df = cleaner.remove_whitespace()
+# df = cleaner.clean_invalid_email()
+
+# print("\nCleaning Done")
+
+# cleaner = DataCleaner(df)
+
+# print("\nMissing Values:\n")
+# print(cleaner.missing_value())
+
+    # print("\nFirst 5 Rows After Cleaning:")
+    # print(df.head())
 
 
 # Export Cleaned Data
-export = ExportData(df)
+# export = ExportData(df)
 
-export.save_data()
-export.save_json()
+# export.save_data()
+# export.save_json()
 
-print("\nCleaned dataset exported successfully!")
+# print("\nCleaned dataset exported successfully!")
